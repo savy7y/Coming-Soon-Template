7906683682cbc9e93df3a5bb9c723d91ef601339
divy <93451471+savy7y@users.noreply.github.com>
2023-04-12 23:33:10 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -12,7 +12,8 @@ var countDownDate = new Date();
 
       // Find the distance between now an the count down date
       var distance = countDownDate - now;
-
+      print(now)
+      print(distance)
       // Time calculations for days, hours, minutes and seconds
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
