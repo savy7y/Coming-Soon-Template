1eb89d6adb4a8de5e9f2834f0208af913a1df119
divy <93451471+savy7y@users.noreply.github.com>
2023-04-12 23:36:32 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
-var countDownDate = new Date('June 25, 2023 03:24:00');
+var countDownDate = new Date();
     // Replace with desired date 'January 17, 2019 03:24:00', example
     // countDownDate.setDate('January 17, 2019 03:24:00').getTime();
-    countDownDate.setDate(25).getTime();
-    
+    countDownDate.setDate(countDownDate.getDate() + 30);
+    countDownDate.getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
@@ -12,8 +12,7 @@ var countDownDate = new Date('June 25, 2023 03:24:00');
 
       // Find the distance between now an the count down date
       var distance = countDownDate - now;
-      print(now)
-      print(distance)
+
       // Time calculations for days, hours, minutes and seconds
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -21,7 +20,6 @@ var countDownDate = new Date('June 25, 2023 03:24:00');
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       
       if (distance > 0) {
-        print("reaching here")
         $("div[name='seconds']").html(seconds);
         $("div[name='minutes']").html(minutes);
         $("div[name='hours']").html(hours);
