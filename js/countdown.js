023172255c55cb032c83b614c4d06b171a0f5273
divy <93451471+savy7y@users.noreply.github.com>
2023-04-12 23:17:57 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 var countDownDate = new Date();
     // Replace with desired date 'January 17, 2019 03:24:00', example
     // countDownDate.setDate('January 17, 2019 03:24:00').getTime();
-    countDownDate.setDate('June 25, 2023 03:24:00');
+    countDownDate.setDate('June 25, 2023 03:24:00').getTime();
     countDownDate.getTime();
 
     // Update the count down every 1 second
